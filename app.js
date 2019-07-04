02b3d326077f8acea6277b78faf742383d8e30df
vasivasista <49262431+vasivasista@users.noreply.github.com>
2019-07-04 16:48:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// //jshint esversion : 6
+// function percent(){
+// var number1=   Number(document.getElementByid("num1").value);
+// alert(number1);
+//
+// }
+document.getElementById('button').addEventListener("click", function(){
+  var number1=   Number(document.getElementById("num1").value);
+  var number2=   Number(document.getElementById("num2").value);
+  result = (number1 * number2)/100;
+
+  alert(`the result is ${result}`);
+});
